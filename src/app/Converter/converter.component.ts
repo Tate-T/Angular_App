c5b8ee6103dafcb207ad9410c2fa0909d21f4282
Tate-T <tatjana.tarasovych@gmail.com>
2022-07-18 23:37:40 +0300

    types of data & methods

--- src/app/Converter/converter.component.ts
+++ src/app/Converter/converter.component.ts
@@ -4,12 +4,35 @@ import { Component } from "@angular/core";
     selector: 'app-converter',
     templateUrl: './converter.component.html',
     styleUrls: ['./converter.component.css'],
-    interpolation: ['{{', '}}']
+    // interpolation: ['{{', '}}']
 })
 
 export class ConverterComponent {
    
-    currencyType = 'USD'
-    currencyResult = ''
-    currencyResultSum = ''
+    currencyType: string = 'USD';
+    currencyResult?:number = 0;
+    currencySum?: string = '';
+
+     handleInputChange (e: string) {
+        const { value } = e.currentTarget;
+        this.currencySum = value;
+      };
+    
+      getAmountBuy () {
+        switch (selectValue.current.value) {
+          case 'USD':
+            this.currencyResult = rates[0].sale * this.currencySum;
+            break;
+          case 'EUR':
+            this. currencyResult = rates[1].sale * this.currencySum;
+            break;
+          default:
+            return;
+        }
+      };
+    
+      onSubmit (e: any) {
+        e.preventDefault();
+        this.getAmountBuy();
+      };
 }
