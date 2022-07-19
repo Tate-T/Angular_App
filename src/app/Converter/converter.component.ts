f0b4fc4e3ef0d6cd9426ba7a915ac26a685bdfa4
Tate-T <tatjana.tarasovych@gmail.com>
2022-07-19 11:18:45 +0300

    hardcode

--- src/app/Converter/converter.component.ts
+++ src/app/Converter/converter.component.ts
@@ -14,7 +14,7 @@ import { HttpClient } from '@angular/common/http';
 
 export class ConverterComponent {
 
-    constructor(private http: HttpClient) { }
+    // constructor(private http: HttpClient) { }
 
     currencyType: string = 'USD';
     currencyResult?:number = 0;
@@ -26,14 +26,14 @@ export class ConverterComponent {
         sale: ""
       };
 
-    rates: any = this.http.get('*.json').subscribe((rates: any) => {
-        return this.response = {
-            ccy: rates.rate.ccy,
-            base_ccy: rates.rate.base_ccy,
-            buy: rates.rate.find((item: any) => item.buy  === 'USD' || 'EUR'),
-            sale: rates.rate.find((item: any) => item.sale === 'USD' || 'EUR'),
-        }
-      });
+    // rates: any = this.http.get('../../data/data.json').subscribe((rates: any) => {
+    //     return this.response = {
+    //         ccy: rates.rate.ccy,
+    //         base_ccy: rates.rate.base_ccy,
+    //         buy: rates.rate.find((item: any) => item.buy  === 'USD' || 'EUR'),
+    //         sale: rates.rate.find((item: any) => item.sale === 'USD' || 'EUR'),
+    //     }
+    //   });
 
     // ngOnInit() {
     //     getCurrencyRateApi().then(response => {
@@ -48,10 +48,12 @@ export class ConverterComponent {
       getAmountBuy (currencySum: any, rates: any) {
         switch (currencySum) {
           case 'USD':
-            this.currencyResult = rates[0].sale * +currencySum;
+            // this.currencyResult = rates[0].sale * +currencySum;
+            this.currencyResult = 37.6000 * +currencySum;
             break;
           case 'EUR':
-            this. currencyResult = rates[1].sale * +currencySum;
+            // this. currencyResult = rates[1].sale * +currencySum;
+            this. currencyResult = 36.20000 * +currencySum;
             break;
           default:
             return;
@@ -61,12 +63,16 @@ export class ConverterComponent {
       getAmountSale (currencySum: any, rates: any) {
         switch (currencySum) {
           case 'USD':
-            this.currencyResult = +currencySum / rates[0].sale;
-            this.currencyType = rates[0].ccy;
+            // this.currencyResult = +currencySum / rates[0].sale;
+            this.currencyResult = +currencySum / 37.20000;
+            // this.currencyType = rates[0].ccy;
+            this.currencyType = "USD";
             break;
           case 'EUR':
-            this.currencyResult = +currencySum / rates[1].sale;
-            this.currencyType = rates[1].ccy;
+            // this.currencyResult = +currencySum / rates[1].sale;
+            this.currencyResult = +currencySum / 36.70000;
+            // this.currencyType = rates[1].ccy;
+            this.currencyType = "EUR";
             break;
           default:
             return;
